f4d72be6928edf70eb1d79644fbbdc3d8ce5d6a0
Tracy-Gregory Gilmore <tracyg.gilmore@gmail.com>
2025-05-24 11:23:57 +0100

    Added web tetris

--- 002-tetris/web.js
+++ 002-tetris/web.js
@@ -1,1 +1,8 @@
+import CONSTANTS from './constants.json' with { type: 'json' };
+const { COLS, ROWS,
+  WEB_KEY_MAPPINGS: KEY_MAPPINGS, 
+  FRAME_DELAY, COLOURS,
+  BASE_HEIGHT, BLOCK_COLOURS } = CONSTANTS;
 
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
